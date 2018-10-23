9368ae658fd2c5db21c1b4048c652d0b25865095
karolkluczniok <karol@codingisthinking.com>
2018-10-23 17:20:59 +0200

    changed user model

--- userauth/apps.py
+++ userauth/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class UserauthConfig(AppConfig):
     name = 'userauth'
--- userauth/models.py
+++ userauth/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser as UserBase
 
-# Create your models here.
+class User(UserBase):
+    profile_image = models.ImageField(upload_to='profile_images', blank=True)
+    
+    def __str__(self):
+        return self.email
