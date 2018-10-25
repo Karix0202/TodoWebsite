65ed8882c07a2b6518db69ba31fe98ddb097e7be
karolkluczniok <karol@codingisthinking.com>
2018-10-25 15:20:28 +0200

    Friend request

--- friends/models.py
+++ friends/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from userauth.models import User
 
-# Create your models here.
+class FriendRequest(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friends_request_sender')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friends_request_receibver')
+    sended = models.DateField(auto_now_add=True)
+    accepted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.sender}|{self.receiver} at {self.sended}'
+    
+    class Meta:
+        ordering = ('sended',)
--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import FriendRequest
 
-# Register your models here.
+@admin.register(FriendRequest)
+class FriendRequestAdmin(admin.ModelAdmin):
+    pass
