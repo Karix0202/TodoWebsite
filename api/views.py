477569c15375f485315054ae7b8ce8a163d477fd
karolkluczniok <karol@codingisthinking.com>
2018-11-03 17:27:10 +0100

    Updated views.py

--- api/views.py
+++ api/views.py
@@ -1,24 +1,19 @@
-from django.contrib.auth import authenticate, logout
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.status import (
-    HTTP_400_BAD_REQUEST,
-    HTTP_404_NOT_FOUND,
-    HTTP_200_OK,
-    HTTP_500_INTERNAL_SERVER_ERROR
-)
 from rest_framework.response import Response
-from .serializers import UserSerializer, FriendRequestSerializer, TodoGroupSerializer
-from userauth.models import User
-from django.db.models import Q
 from rest_framework.views import APIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.viewsets import ViewSet
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_200_OK, HTTP_500_INTERNAL_SERVER_ERROR
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, logout
+from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from friends.models import FriendRequest
-from rest_framework.authentication import TokenAuthentication
+from userauth.models import User
 from todo.models import TodoGroup
-from rest_framework.viewsets import ViewSet
+from .serializers import UserSerializer, FriendRequestSerializer, TodoGroupSerializer
 
 
 @csrf_exempt
