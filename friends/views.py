4dc748bdb9f7696cd978a48d50ce7a95f16e5e9a
karolkluczniok <karol@codingisthinking.com>
2018-11-19 16:03:42 +0100

    Fixes

--- friends/views.py
+++ friends/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.views import View
 from .forms import AddFriendForm
-from django.http import HttpResponse, JsonResponse, Http404
+from django.http import HttpResponse
 from django.db.models import Q
 from userauth.models import User
 from .models import FriendRequest
 from django.contrib.auth.decorators import login_required
+
 
 class SendFriendRequestView(View):
     def get(self, request):
@@ -19,32 +20,12 @@ class SendFriendRequestView(View):
 
         if form.is_valid():
             username = form.cleaned_data['username']
-            users = self.get_users(request, username)
-            ctx['users'] = users
+            queryset = User.objects.filter(Q(username__icontains=username)).exclude(pk=request.user.pk).exclude(
+                id__in=[u.pk for u in request.user.friends.all()])
+            ctx['users'] = queryset
 
         return render(request, 'friends/add_friend.html', ctx)
 
-    def get_user_by_his_username(self, request, username):
-        return User.objects.filter(
-            Q(username__icontains=username)
-        ).exclude(username=request.user.username)
-        
-    def are_users_friends(self, request, user):
-        sended = FriendRequest.objects.filter(sender=request.user).filter(receiver=user)
-        received = FriendRequest.objects.filter(sender=request.user).filter(receiver=request.user)
-        return True if len(sended) > 0 or len(received) > 0 else False
-
-    def get_proper_users(self, request, username):
-        users = self.get_user_by_his_username(request, username)
-        for user in users:
-            if not self.are_users_friends(request, user):
-                yield {
-                    'username': user.username,
-                    'id': user.id,
-                }
-
-    def get_users(self, request, username):
-        return [user for user in self.get_proper_users(request, username)]
 
 @login_required
 def add_friend(request, id):
@@ -53,7 +34,9 @@ def add_friend(request, id):
 
     receiver = User.objects.filter(id=id).first()
 
-    if FriendRequest.objects.filter(receiver=receiver).filter(sender=request.user).first() is not None or FriendRequest.objects.filter(sender=receiver).filter(receiver=request.user).first() is not None:
+    if FriendRequest.objects.filter(receiver=receiver).filter(
+            sender=request.user).first() is not None or FriendRequest.objects.filter(sender=receiver).filter(
+            receiver=request.user).first() is not None:
         return HttpResponse(status=500)
 
     friend_request = FriendRequest(receiver=receiver, sender=request.user)
@@ -61,10 +44,11 @@ def add_friend(request, id):
 
     return redirect(reverse('friends:find'))
 
+
 @login_required
 def index(request):
     waiting_for_accept = FriendRequest.objects.filter(receiver=request.user).filter(accepted=False)
-    sended = FriendRequest.objects.filter(sender=request.user).filter(accepted=False)
+    sent = FriendRequest.objects.filter(sender=request.user).filter(accepted=False)
     friends = FriendRequest.objects.filter(
         Q(receiver=request.user) |
         Q(sender=request.user)
@@ -72,26 +56,18 @@ def index(request):
 
     return render(request, 'friends/list.html', {
         'waiting_for_accept': waiting_for_accept,
-        'sended': sended,
+        'sent': sent,
         'friends': friends,
     })
+
 
 @login_required
 def cancel_request(request, id):
     req = get_object_or_404(FriendRequest, pk=id)
 
-    # if req.accepted is True:
-    #     sender = req.sender
-    #     receiver = req.receiver
-
-    #     sender.friends.remove(receiver)
-    #     receiver.friends.remove(sender)
-        
-    #     sender.save()
-    #     receiver.save()
-
     req.delete()
     return redirect(reverse('friends:index'))
+
 
 @login_required
 def accept(request, id):
@@ -102,13 +78,5 @@ def accept(request, id):
 
     friend_request.accepted = True
     friend_request.save()
-
-    # user = request.user
-    # user.friends.add(friend_request.sender)
-    # user.save()
-
-    # friend = friend_request.sender
-    # friend.friends.add(user)
-    # friend.save()
 
     return redirect(reverse('friends:index'))
